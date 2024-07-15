c4c71bcb880ef643ca1015a3548d2d0da1b8fdcc
Pat <patb.software@gmail.com>
2024-07-15 23:28:54 +0700

    Remove console.log statements

--- sum-of-two.js
+++ sum-of-two.js
@@ -9,7 +9,7 @@
 // Input: [1, 3, 5, 7, 16, 4], Target: 18, Output: false.
 // Input: [12, 17, 14, 11, 19, 8], Target: 20 Output: true
 
-// NOTE: I will assume that the two integers should not belong to the same array index.
+// Note: I will assume that the two integers should not belong to the same array index.
 function sumOfTwo(integers, target) {
     // Use a hashmap to store value-index pairs in the array for quick access
     const map = new Map();
@@ -25,8 +25,3 @@ function sumOfTwo(integers, target) {
 
     return false;
 }
-
-console.log(sumOfTwo([1, 3, 5, 7, 16, 4], 8));
-console.log(sumOfTwo([1, 3, 5, 7, 16, 4], 12));
-console.log(sumOfTwo([1, 3, 5, 7, 16, 4], 18));
-console.log(sumOfTwo([12, 17, 14, 11, 19, 8], 20));
